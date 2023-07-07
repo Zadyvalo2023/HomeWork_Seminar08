d06e1e16dac9b6f06ae570e7a92130ba67d0221f
Zadyvalo2023 <av1469400@gmail.com>
2023-07-07 09:23:15 +0300

    My Home work

---
+++ Task058/Program.cs
@@ -0,0 +1,60 @@
+﻿        // Задача 58: Задайте две матрицы. Напишите программу,
+        // которая будет находить произведение двух матриц.
+        // Например, даны 2 матрицы:
+        // 2 4 | 3 4
+        // 3 2 | 3 3
+        // Результирующая матрица будет:
+        // 18 20
+        // 15 18
+
+        //=========================================================
+
+using System;
+ class Program
+{
+    static void Main()
+    {
+        // Задаем две матрицы
+        int[,] matrix1 = new int[,]
+        {
+            { 2, 4 },
+            { 3, 2 }
+        };
+         int[,] matrix2 = new int[,]
+        {
+            { 3, 4 },
+            { 3, 3 }
+        };
+         // Проверяем, можно ли умножить матрицы
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+        {
+            Console.WriteLine("Невозможно умножить матрицы");
+            Console.ReadLine();
+            return;
+        }
+         // Создаем результирующую матрицу
+        int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+         // Выполняем умножение матриц
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+         // Выводим результирующую матрицу
+        Console.WriteLine("Результирующая матрица:");
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+         Console.ReadLine();
+    }
+}
---
+++ Task062/Program.cs
@@ -0,0 +1,60 @@
+﻿    // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+    // Например, на выходе получается вот такой массив:
+    // 01 02 03 04
+    // 12 13 14 05
+    // 11 16 15 06
+    // 10 09 08 07
+
+    //================================================
+
+using System;
+ class Program
+{
+    static void Main()
+    {
+        int n = 4;
+        int[,] array = new int[n, n];
+         int value = 1;
+        int startX = 0;
+        int startY = 0;
+        int endX = n - 1;
+        int endY = n - 1;
+         while (value <= n * n)
+        {
+            // Вправо
+            for (int i = startX; i <= endX; i++)
+            {
+                array[startY, i] = value++;
+            }
+            startY++;
+             // Вниз
+            for (int i = startY; i <= endY; i++)
+            {
+                array[i, endX] = value++;
+            }
+            endX--;
+             // Влево
+            for (int i = endX; i >= startX; i--)
+            {
+                array[endY, i] = value++;
+            }
+            endY--;
+             // Вверх
+            for (int i = endY; i >= startY; i--)
+            {
+                array[i, startX] = value++;
+            }
+            startX++;
+        }
+         // Выводим массив
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{array[i, j]:D2} ");
+            }
+            Console.WriteLine();
+        }
+         Console.ReadLine();
+    }
+}
